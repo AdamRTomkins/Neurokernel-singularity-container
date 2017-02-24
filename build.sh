a270ba386247a8390a3f38e8bc657a85ad77d7f5
Adam Tomkins <artomkins@googlemail.com>
2017-02-24 19:30:05 +0000

    add example to run

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ sudo singularity create $IMAGE
 sudo singularity expand --size 5000 $IMAGE
 sudo singularity bootstrap $IMAGE $DEF
 sudo singularity exec -B `pwd`:/mnt -w $IMAGE sh /mnt/install_nvidia.sh
+
+
+#sudo singularity exec -B `pwd`:/mnt -w neurokernel.img sh /mnt/install_nvidia.sh
