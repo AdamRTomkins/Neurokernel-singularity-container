baa777f455bb86b15ff8ce762173cc6737fbb622
Adam Tomkins <artomkins@googlemail.com>
2017-03-28 15:39:34 +0100

    Update runscript and examples

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 export PATH="/miniconda/bin:${PATH}"
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/nvidia:${LD_LIBRARY_PATH}
 
+
+export PATH="/usr/local/nvidia/bin:/usr/local/cuda/bin:${PATH}"
+export LD_LIBRARY_PATH=/usr/local/nvidia/lib:/usr/local/nvidia/lib64:${LD_LIBRARY_PATH}
+
 cd examples/data/
 python gen_generic_lpu.py
 cd ../
 python generic_demo.py --log=screen
-python visualize_output.py
 
