d53902adc83242fce2f328910ea699a144d76747
Adam Tomkins <artomkins@googlemail.com>
2017-04-13 12:09:05 +0100

    reduce build size further

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ IMAGE="neurokernel.img"
 DEF="Singularity"
 sudo rm -f $IMAGE
 sudo singularity create $IMAGE
-sudo singularity expand --size 4000 $IMAGE
+sudo singularity expand --size 3000 $IMAGE
 sudo singularity bootstrap $IMAGE $DEF
 
